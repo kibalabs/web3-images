522a449918c30d3892f6a7652fbd64cd6b17f9f6
Krishan Patel <krishan711@gmail.com>
2021-11-12 15:57:35 +0000

    Feature: initial commit (#1)
    * .
    
    * .

---
+++ app/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { useFavicon } from '@kibalabs/core-react';
+import { Alignment, BackgroundView, Direction, Head, KibaApp, Stack, Text } from '@kibalabs/ui-react';
+
+import { GlobalsProvider } from './globalsContext';
+import { buildAppTheme } from './theme';
+
+const theme = buildAppTheme();
+
+const globals = {
+};
+
+const defaultDate = new Date();
+defaultDate.setHours(0, 0, 0, 0);
+
+export const App = (): React.ReactElement => {
+  useFavicon('/assets/favicon.svg');
+
+  return (
+    <KibaApp theme={theme}>
+      <Head headId='app'>
+        <title>Web3 Images</title>
+      </Head>
+      <GlobalsProvider globals={globals}>
+        <BackgroundView linearGradient='#36D1DC,#1C92D2'>
+          <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Start} isScrollableVertically={true}>
+            <Text variant='header1'>Web3 Images</Text>
+          </Stack>
+        </BackgroundView>
+      </GlobalsProvider>
+    </KibaApp>
+  );
+};
