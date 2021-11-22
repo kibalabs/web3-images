c5c2f30bffcc20c4c0a9a0071ba3131de25cdf00
Krishan Patel <krishan711@gmail.com>
2021-11-22 12:53:39 +0000

    .

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -1,243 +1,92 @@
 import { buildTheme, ITheme } from '@kibalabs/ui-react';
 
-const defaultTheme = buildTheme();
 export const buildAppTheme = (): ITheme => {
   const theme = buildTheme({
     colors: {
-      brandPrimary: '#6F0000',
-      brandSecondary: '#200122',
+      brandPrimary: '#1C92D2',
+      brandSecondary: '#36D1DC',
       background: '#000000',
       text: '#ffffff',
-      placeholderText: 'rgba(255, 255, 255, 0.5)',
     },
     fonts: {
       main: {
-        url: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800,900&display=swap',
-      },
-    },
-    links: {
-      default: {
-        normal: {
-          default: {
-            text: {
-              color: '$colors.text',
-            },
-          },
-          hover: {
-            text: {
-              color: '$colors.textDark10',
-            },
-          },
-        },
-        visited: {
-          default: {
-            text: {
-              color: '$colors.textDark15',
-            },
-          },
-        },
+        url: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap',
       },
     },
     texts: {
       default: {
-        'font-family': "'Open Sans', sans-serif",
+        'font-family': "'Roboto Condensed', sans-serif",
         'font-weight': '400',
       },
-      header3: {
-        'font-size': '1.5rem',
-        'font-weight': '600',
-      },
-      subtitle: {
-        'font-size': '0.75rem',
-        'font-weight': '600',
-      },
-      account: {
-        'font-size': '1.5rem',
-        'font-weight': '600',
-        'font-family': "'Open Sans', sans-serif",
-        padding: '20px',
-      },
-      normal: {
-        'font-size': '0.85rem',
-        'padding-Right': '20px',
-      },
-      small: {
-        'font-size': '0.85rem',
-      },
-      light: {
-        color: 'rgba(255, 255, 255, 0.75)',
-      },
-      cardLabel: {
-        color: 'rgba(255, 255, 255, 0.75)',
-      },
-      cardLabelSponsored: {
-        color: 'rgba(238, 213, 102, 1)',
-      },
-      cardLabelRandom: {
-        color: 'rgba(46, 180, 255, 1)',
-      },
-    },
-    boxes: {
-      phBanner: {
-        padding: `${defaultTheme.dimensions.paddingWide} ${defaultTheme.dimensions.paddingWide}`,
-        'background-color': 'rgba(0, 0, 0, 0.5)',
-      },
-      card: {
-        padding: '0',
-        'background-color': 'rgba(255, 255, 255, 0.15)',
-        'border-width': '0',
-        // 'border-width': '1px',
-        // 'border-color': 'rgba(255, 255, 255, 0.5)',
-        // 'border-style': 'solid',
-        margin: '0',
-      },
-      cardLabelBox: {
-        'border-radius': '0.5em 0 0.2em 0',
-        padding: '0.5em 1em',
-      },
-      cardLabelBoxSponsored: {
-        'background-color': 'rgba(238, 213, 102, 0.25)',
-      },
-      cardLabelBoxRandom: {
-        'background-color': 'rgba(46, 180, 255, 0.25)',
+      note: {
+        color: '$colors.textClear50',
       },
     },
     iconButtons: {
       default: {
+        normal: {
+          default: {
+            background: {
+              'border-width': '1px',
+              'border-color': 'rgba(255, 255, 255, 0.7)',
+              'background-color': 'rgba(255, 255, 255, 0.2)',
+            },
+          },
+          hover: {
+            background: {
+              'border-color': 'rgba(255, 255, 255, 0.8)',
+            },
+          },
+          press: {
+            background: {
+              'border-color': 'rgba(255, 255, 255, 0.9)',
+            },
+          },
+          focus: {
+            background: {
+              'border-color': 'rgba(255, 255, 255, 1)',
+            },
+          },
+        },
         disabled: {
           default: {
             background: {
-              'background-color': 'rgba(255, 255, 255, 0)',
-            },
-          },
-        },
-      },
-    },
-    buttons: {
-      primary: {
-        normal: {
-          default: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.25)',
-              'border-color': 'rgba(255, 255, 255, 0.3)',
               'border-width': '1px',
+              'border-color': 'rgba(255, 255, 255, 0.2)',
+              'background-color': 'rgba(255, 255, 255, 0.1)',
             },
             text: {
-              color: '$colors.textOnBrand',
-            },
-          },
-          hover: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.35)',
-            },
-          },
-          press: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.55)',
-            },
-          },
-          focus: {
-            background: {
-              'border-color': 'rgba(255, 255, 255, 0.75)',
+              color: 'rgba(255, 255, 255, 0.2)',
             },
           },
         },
       },
-      secondary: {
-        normal: {
-          default: {
-            background: {
-              'border-color': 'rgba(255, 255, 255, 0.3)',
-              'border-width': '1px',
-            },
-            text: {
-              color: '$colors.textOnBrand',
-            },
-          },
-          hover: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.35)',
-            },
-          },
-          press: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.55)',
-            },
-          },
-          focus: {
-            background: {
-              'border-color': 'rgba(255, 255, 255, 0.75)',
-            },
-          },
-        },
-      },
-      tertiary: {
-        normal: {
-          default: {
-            background: {
-              'border-width': '0',
-            },
-            text: {
-              color: '$colors.textOnBrand',
-            },
-          },
-          hover: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.35)',
-            },
-          },
-          press: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.55)',
-            },
-          },
-          focus: {
-            background: {
-              'border-color': 'rgba(255, 255, 255, 0.75)',
-            },
-          },
-        },
-      },
-      buttonPlay: {
-        normal: {
-          default: {
-            background: {
-              'border-width': '1px',
-              'border-color': '#000000',
-            },
-            text: {
-              color: '$colors.textOnBrand',
-            },
-          },
-          hover: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.35)',
-            },
-          },
-          press: {
-            background: {
-              'background-color': 'rgba(255, 255, 255, 0.55)',
-            },
-          },
-          focus: {
-            background: {
-              'border-color': 'rgba(255, 255, 255, 0.75)',
-            },
-          },
-        },
-
-      },
-    },
-    input: {
-      'background-color': '#36D1DC',
-
     },
     inputWrappers: {
-      dialogInput: {
+      default: {
         normal: {
           default: {
             background: {
-              'background-color': 'rgba(255, 255, 255, 0.25)',
+              'border-width': '1px',
+              'border-color': 'rgba(255, 255, 255, 0.7)',
+              'background-color': 'rgba(255, 255, 255, 0.2)',
+            },
+            placeholderText: {
+              color: 'rgba(255, 255, 255, 0.5)',
+            },
+          },
+          hover: {
+            background: {
+              'border-width': '1px',
+              'border-color': 'rgba(255, 255, 255, 0.8)',
+              'background-color': 'rgba(255, 255, 255, 0.2)',
+            },
+          },
+          focus: {
+            background: {
+              'border-width': '1px',
+              'border-color': 'rgba(255, 255, 255, 1)',
+              'background-color': 'rgba(255, 255, 255, 0.2)',
             },
           },
         },
