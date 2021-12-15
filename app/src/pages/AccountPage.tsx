510bfa30c267352694c620e42a9440b9dbdb430a
Barakat Ajadi <barakat.a.ajadi@gmail.com>
2021-12-15 13:17:46 +0100

    add useRouteparams

--- app/src/pages/AccountPage.tsx
+++ app/src/pages/AccountPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useRouteParams } from '@kibalabs/core-react';
 import { Alignment, Box, Direction, Image, LinkBase, PaddingSize, ResponsiveContainingView, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 import { SponsorView } from '../components/Sponsors';
@@ -9,8 +10,11 @@ export type AccountPageProps = {
   accountId: string;
 }
 
-export const AccountPage = (props: AccountPageProps): React.ReactElement => {
+export const AccountPage = (): React.ReactElement => {
   const { apiUrl } = useGlobals();
+  const routeParams = useRouteParams();
+  const accountId = routeParams.accountId as string;
+
   return (
     <ResponsiveContainingView sizeResponsive={{ base: 12, medium: 10, large: 8 }}>
       <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} padding={PaddingSize.Wide2} shouldAddGutters={true}>
@@ -21,16 +25,16 @@ export const AccountPage = (props: AccountPageProps): React.ReactElement => {
           <Spacing variant={PaddingSize.Wide2} />
         </Stack.Item>
         <Stack.Item gutterAfter={PaddingSize.Wide}>
-          <Text variant='header3'>{props.accountId}</Text>
+          <Text variant='header3'>{accountId}</Text>
         </Stack.Item>
         <Box width='250px' height='250px'>
           <Image
-            source={`${apiUrl}/v1/accounts/${props.accountId}/image`}
+            source={`${apiUrl}/v1/accounts/${accountId}/image`}
             alternativeText='Avatar'
             // onError={(error: React.SyntheticEvent): void => { error.target.onerror = null; error.target.src = '/assets/tokenhunt.jpg'; }}
           />
         </Box>
-        <Text alignment={TextAlignment.Center}>{`via api: ${apiUrl}/v1/accounts/${props.accountId}/image`}</Text>
+        <Text alignment={TextAlignment.Center}>{`via api: ${apiUrl}/v1/accounts/${accountId}/image`}</Text>
         <Stack.Item growthFactor={1} shrinkFactor={1}>
           <Spacing variant={PaddingSize.Wide2} />
         </Stack.Item>
--- app/src/pages/TokenPage.tsx
+++ app/src/pages/TokenPage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useRouteParams } from '@kibalabs/core-react';
 import { Alignment, Box, Direction, Image, LinkBase, PaddingSize, ResponsiveContainingView, Spacing, Stack, Text, TextAlignment } from '@kibalabs/ui-react';
 
 import { SponsorView } from '../components/Sponsors';
@@ -10,8 +11,12 @@ export type TokenPageProps = {
   tokenId: string;
 }
 
-export const TokenPage = (props: TokenPageProps): React.ReactElement => {
+export const TokenPage = (): React.ReactElement => {
   const { apiUrl } = useGlobals();
+  const routeParams = useRouteParams();
+  const registryAddress = routeParams.registryAddress as string;
+  const tokenId = routeParams.tokenId as string;
+
   return (
     <ResponsiveContainingView sizeResponsive={{ base: 12, medium: 10, large: 8 }}>
       <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} padding={PaddingSize.Wide2} shouldAddGutters={true}>
@@ -22,16 +27,16 @@ export const TokenPage = (props: TokenPageProps): React.ReactElement => {
           <Spacing variant={PaddingSize.Wide2} />
         </Stack.Item>
         <Stack.Item gutterAfter={PaddingSize.Wide}>
-          <Text variant='header3'>{`${props.registryAddress}/${props.tokenId}`}</Text>
+          <Text variant='header3'>{`${registryAddress}/${tokenId}`}</Text>
         </Stack.Item>
         <Box width='250px' height='250px'>
           <Image
-            source={`${apiUrl}/v1/collections/${props.registryAddress}/tokens/${props.tokenId}/image`}
+            source={`${apiUrl}/v1/collections/${registryAddress}/tokens/${tokenId}/image`}
             alternativeText='Avatar'
             // onError={(error: React.SyntheticEvent): void => { error.target.onerror = null; error.target.src = '/assets/tokenhunt.jpg'; }}
           />
         </Box>
-        <Text alignment={TextAlignment.Center}>{`via api: ${apiUrl}/v1/collections/${props.registryAddress}/tokens/${props.tokenId}/image`}</Text>
+        <Text alignment={TextAlignment.Center}>{`via api: ${apiUrl}/v1/collections/${registryAddress}/tokens/${tokenId}/image`}</Text>
         <Stack.Item growthFactor={1} shrinkFactor={1}>
           <Spacing variant={PaddingSize.Wide2} />
         </Stack.Item>
